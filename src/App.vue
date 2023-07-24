7759f675a931d46a3ba45a07766395a9c1cde17d
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-07-25 01:01:31 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import sidebar from "./components/sidebar.vue";
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    sidebar,
+    Header,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <Header />
+    <sidebar />
+  </div>
+</template>
+
+<style scoped></style>
